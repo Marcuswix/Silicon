7c2d614a92bf4124efc5c81df25c1ae2d9411bd0
Marcus <Marcuswix@yahoo.se>
2024-02-21 16:58:00 +0100

    Contact

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const toggleButton = document.getElementById("hamburger");
+const menu = document.getElementById("menu-mobil");
+const main = document.getElementById("main");
+const footer = document.getElementById("footer");
+const contactForm = document.getElementById("contact-us");
+
+// En "eventlyssnare" för klickhändelser på hela dokumentet
+document.addEventListener('click', (event) => {
+  const target = event.target;
+ 
+  if (target !== menu && target !== toggleButton) 
+  {
+    if (menu.classList.contains('open')) {
+      menu.classList.remove('open');
+      toggleButton.classList.remove('fa-xmark');
+      toggleButton.classList.add('fa-bars');
+      main.classList.remove('overlay')
+    }
+  }
+});
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle('open');
+
+  if(menu.classList.contains('open')) 
+  {
+    toggleButton.classList.remove('fa-bars');
+    toggleButton.classList.add('fa-xmark');
+    main.classList.add('overlay');
+    footer.classList.add('overlay');
+  } 
+  else 
+  {
+    toggleButton.classList.remove('fa-xmark');
+    toggleButton.classList.add('fa-bars');
+    main.classList.remove('overlay')
+    footer.classList.remove('overlay')
+  }
+});
+
+function showHideMessage()
+{
+  if(contactForm.classList.contains("hide"))
+  {
+    contactForm.classList.remove("hide")
+  }
+  else
+  {
+    contactForm.classList.add("hide")
+  }
+}
